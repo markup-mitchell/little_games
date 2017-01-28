f8bdce1dca667e68b0b02cf904c7f9f9acc14aaf
spitchell <mr.spitchell@gmail.com>
2017-01-28 11:19:41 +0000

    guessed letters appear in turn

--- spelling/js-spelling.js
+++ spelling/js-spelling.js
@@ -1,43 +1,61 @@
 // todo
-// * array of names
-// * associate name with image
-// * display empty letterboxes (style invisible letters)
-// * color border for letter next expected
-// * function to accept/reject
+// - [X] array of names
+// - [ ]associate name with image
+// - [X] display empty letterboxes (style invisible letters)
+// - [ ] color border for letter next expected
+// - [ ] function to accept/reject
 
+function wholeThing(){
 
+  var wordWrapper = document.querySelector('.wordWrapper');
 
-var wordWrapper = document.querySelector('.wordWrapper');
+  const wordArray = ["keir", "daddy", "mummy", "nancy", "johnny", "grandad", "iris", "noah", "rory"]; 
 
-const wordArray = ["keir", "daddy", "mummy", "nancy", "johnny", "grandad", "iris", "noah", "rory"]; 
+  var letterArray = randomPick().split("");
 
-function randomPick() {
+  function randomPick() {
       let choice = Math.floor(Math.random()* wordArray.length);
+      console.log(wordArray[choice]);
    return wordArray[choice];
-}
+  } // END randomPick
 
-var letterArray = randomPick().split("");
+// generate word to guess 
+  for (i=0; i<letterArray.length; i++) {
+     let newLetterSpace = document.createElement("div");  
+     let displayLetter = document.createTextNode(letterArray[i]); // k
+     let pTag = document.createElement("p"); // <p></p>
+     pTag.appendChild(displayLetter); // <p>k</p>
+     newLetterSpace.append(pTag);
+     wordWrapper.append(newLetterSpace);
+     newLetterSpace.classList.add("letterBox");
+     newLetterSpace.classList.add("hidden");
+  } // END word-generating for loop
 
-for (i=0; i<letterArray.length; i++) {
-   let newLetterSpace = document.createElement("div");  
-   
-   let displayLetter = document.createTextNode(letterArray[i]); // k
-   let pTag = document.createElement("p"); // <p></p>
-   pTag.appendChild(displayLetter); // <p>k</p>
-   newLetterSpace.append(pTag);
-   wordWrapper.append(newLetterSpace);
-   newLetterSpace.classList.add("letterBox");
-}
-window.addEventListener("keydown", function (event) {  
-   console.log(event.key);
-   if (event.key == blankWord[i].innerHTML) {
-         console.log("CORRECT!")
-   }
-})
+// get array of <p> elements from DOM - letters in word
+  var blankWord = document.querySelectorAll("p");
+
+  var letterIndex = 0;
+  var currentLetter = letterArray[letterIndex];
+
+  var boxList = wordWrapper.children;
+  console.log(boxList);
+
+window.addEventListener("keydown", function (event){
+  console.log(currentLetter);
+  console.log(event.key);
+  if (event.key !== currentLetter) {
+    console.log("WRONG! TRY AGAIN!");
+  }
+  else {console.log("CORRECT!")
+  boxList[letterIndex].classList.add("reveal");
+  letterIndex++;
+  if (letterIndex == blankWord.length) {
+    console.log("YOU WIN!!!");
+  }
+  currentLetter = letterArray[letterIndex];
+  }
+});
 
 
-var blankWord = document.querySelectorAll("p");
 
-var currentLetter = blankWord[0].innerHTML;
-
-console.log(currentLetter);
+} // END wholeThing
