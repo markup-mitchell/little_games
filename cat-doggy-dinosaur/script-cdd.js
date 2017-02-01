9de29a72882ec0b42ce09ba63e3a0881c307c87a
spitchell <mr.spitchell@gmail.com>
2017-02-01 08:52:22 +0000

    object logic integrated

--- cat-doggy-dinosaur/script-cdd.js
+++ cat-doggy-dinosaur/script-cdd.js
@@ -1,64 +1,35 @@
 function wholeThing() {
 
+//++++++++++++++++++++++++++++++++++++++++++//
+//                  VARIABLES               //
+//++++++++++++++++++++++++++++++++++++++++++//
 
+const cat =  {
+name: "cat", //this may be unnecessary?!?//
+sound: "meow",
+clips: 5 // number of sound samples available
+};
 
-//++++++++++++++++++++++++++++++++++++++++++++++//
-//            SOUND VARIABLE ARRAYS             //
-//++++++++++++++++++++++++++++++++++++++++++++++//
+const dog = {
+  name: "dog",
+  sound: "woof",
+  clips: 6
+};
 
-const cat = [ // a good/legit way to lay these out? //
-  "meow-1",
-  "meow-3",
-  "meow-4",
-  "meow-5",
-//  "meow-6",
-//  "meow-7",
-//  "meow-8",
-//  "meow-9",
-//  "meow-10",
-//  "meow-11"
-  ];
+const dinosaur = {
+    name: "dinosaur",
+    sound: "roar",
+    clips: 7
+};
 
-const dog = [ // a good/legit way to lay these out? //
-  "woof-1",
-  "woof-3",
-  "woof-4",
-//  "woof-5",
-//  "woof-6",
-//  "woof-7",
-//  "woof-8",
-//  "woof-9",
-//  "woof-10",
-// "woof-11"
-];
-
-const dinosaur = [
-  "roar-1",
-  "roar-3",
-  "roar-4",
-  "roar-5",
-  "roar-6",
-  "roar-7",
-//  "roar-8",
-//  "roar-9",
-//  "roar-10",
-//  "roar-11"
-];
-
-var species = [ // one for each sound array, obvs
-  cat,
-  dog,
-  dinosaur
-  ];
-
+const beastList = [cat, dog, dinosaur];
 
 //++++++++++++++++++++++++++++++++++++++++++//
 //                  FUNCTIONS               //
 //++++++++++++++++++++++++++++++++++++++++++//
 
-function pickRandom(array) {
-    let randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
+function pickRandom(max){
+  return Math.floor(Math.random() * max) + 1;
 }
 
 function removeTransition(e) {
@@ -67,12 +38,14 @@ function removeTransition(e) {
 }
 
 function listenAndGuess() { // choose a sound and require input to continue //
-    let currentSound = pickRandom(pickRandom(species));
-    let audioClip = new Audio("sounds/" + currentSound + ".wav");
+   
+    let beast = beastList[pickRandom(beastList.length)-1];
+console.log(beast.name);
+    let clipNumber =  beast.clips;
+    let currentSound = new Audio ("sounds/" + beast.sound + "-"+ pickRandom(clipNumber) + ".wav");
+console.log(currentSound);
+    currentSound.play();
     this.classList.add("playing");
-    console.log(audioClip);
-    audioClip.play();
-
 // let userChoice = catBox.addEventListener("click", function(){return "cat"}); 
 //  console.log(userChoice);
 }
@@ -90,4 +63,4 @@ console.log(earBox);
 earBox.addEventListener("click", listenAndGuess);
 
 
-}
+} /* END function wholeThing */
